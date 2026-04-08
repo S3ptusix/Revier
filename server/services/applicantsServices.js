8a94880aaacd7b8200deb00f135949ab418c6afd
Septusix <jahleelnemuel@gmail.com>
2026-04-08 14:07:35 +0800

    feat: add interview result handling and update applicant services for orientationId

--- server/services/applicantsServices.js
+++ server/services/applicantsServices.js
@@ -111,6 +111,7 @@ export const fetchApplicantPipelineService = async (adminId) => {
                 'id',
                 'fullname',
                 'phone',
+                'orientationId',
                 'orientationStatus',
                 'blacklistedReason',
             ],
