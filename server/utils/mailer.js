54698c47af2829b6dd95e4da17c098a709567085
Septusix <jahleelnemuel@gmail.com>
2026-07-21 14:09:28 +0800

    fix: update mail sender to use environment variable for Resend email

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -24,7 +24,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendMail = async ({ to, subject, html }) => {
   try {
     const response = await resend.emails.send({
-      from: 'onboarding@resend.dev', // default test sender
+      from: process.env.RESEND_EMAIL,
       to,
       subject,
       html,
